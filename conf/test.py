70d74e0d337fda9202ec7f1e219aa2ef400761f2
AdamTheisen <atheisen@anl.gov>
2023-04-12 18:54:30 +0000

    Cronjob commit

--- conf/test.py
+++ conf/test.py
@@ -14,6 +14,6 @@ conf = {
     'dqr_table': True,
     'doi_table': True, #this will remove the DOI from besides the plots
     'instruments':{
-        'sonde': {'dsname': 'sondewnpnM1.b1', 't_delta': 1440./4},
+        'sonde': {'dsname': 'sondewnpnM1.b1', 't_delta': 1440./2},
     }
 }
