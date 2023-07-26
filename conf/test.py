e27e9061b42e735d8e16175e814b264608c7aacb
AdamTheisen <atheisen@anl.gov>
2023-07-26 17:19:11 +0000

    NEW Images

--- conf/test.py
+++ conf/test.py
@@ -3,17 +3,16 @@
 # t_delta: Script will resample data to 1 min.  If gaps are longer set this appropriately
 # workers: Set to 1 in the case of radar data so it doesn't crash the system
 conf = {
-    'site': 'epc',
+    'site': 'guc',
     'facility': 'M1',
-    'start_date': '2023-02-15',
-    'end_date': '2023-04-14',
-    'outname': '/home/theisen/Code/AFC_Summary/images/test.pdf', #options are png, pdf, etc
+    'start_date': '2021-09-01',
+    'end_date': '2023-06-15',
+    'outname': '/home/theisen/Code/AFC_Summary/images/guc_dl_test.pdf', #options are png, pdf, etc
     'chart_style': 'linear',
     'info_style': 'simple',
-    'data_path': '/data/archive',
     'dqr_table': True,
     'doi_table': True, #this will remove the DOI from besides the plots
     'instruments':{
-        'sp2': {'dsname': 'aossp2bc60sM1.b1'},
+        'dl': {'dsname': 'dlfptM1.b1', 'dsname2': 'dlppiM1.b1'},
     }
 }
