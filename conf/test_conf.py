b32fca9e53580d69a4380c401b6021c9950f532e
AdamTheisen <atheisen@anl.gov>
2025-02-24 20:14:14 +0000

    ENH: Testing

--- conf/test_conf.py
+++ conf/test_conf.py
@@ -3,8 +3,8 @@
 # t_delta: Script will resample data to 1 min.  If gaps are longer set this appropriately
 # workers: Set to 1 in the case of radar data so it doesn't crash the system
 conf = {
-    'site': 'bnf',
-    'facility': 'M1',
+    'site': 'crg',
+    'facility': 'S2',
     'start_date': '2024-10-01',
     'end_date': '2025-10-01',
     'outname': '/home/theisen/Code/AFC_Summary/images/test.pdf', #options are png, pdf, etc
@@ -14,6 +14,6 @@ conf = {
     'dqr_table': True,
     'doi_table': True, #this will remove the DOI from besides the plots
     'instruments':{
-        'ccn': {'dsname': 'aosccn2colaavgM1.b1', 't_delta': 20},
+        'dl': {'dsname': 'dlfptS2.b1', 't_delta': 30},
     }
 }
