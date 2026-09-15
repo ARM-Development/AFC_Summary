0b64ee6536b7a6812216f7dd66b3774734063303
AdamTheisen <atheisen@anl.gov>
2026-09-15 15:48:29 +0000

    NEW test

--- conf/test_conf.py
+++ conf/test_conf.py
@@ -3,17 +3,17 @@
 # t_delta: Script will resample data to 1 min.  If gaps are longer set this appropriately
 # workers: Set to 1 in the case of radar data so it doesn't crash the system
 conf = {
-    'site': 'crg',
-    'facility': 'S2',
-    'start_date': '2024-12-01',
-    'end_date': '2025-12-01',
-    'outname': '/home/theisen/Code/AFC_Summary/images/test.pdf', #options are png, pdf, etc
+    'site': 'kcg',
+    'facility': 'M1',
+    'start_date': '2024-04-15',
+    'end_date': '2025-10-15',
+    'outname': '/home/theisen/Code/AFC_Summary/images/testdata_avail.pdf', #options are png, pdf, etc
     'chart_style': 'linear',
     'info_style': 'simple',
     'data_path': '/data/archive',
     'dqr_table': True,
     'doi_table': True, #this will remove the DOI from besides the plots
     'instruments':{
-        'dl': {'dsname': 'dlfptS2.b1', 't_delta': 1},
+        'sirs': {'dsname': 'sirsM1.b1'},
     }
 }
